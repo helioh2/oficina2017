87b6cd11c19a536c4fdad87058dbe868c53a3692
Helio <heliohenrique@ufpr.br>
2017-08-25 18:44:53 -0300

    Exercício foguete.

---
+++ exercicios3/foguete.py
@@ -0,0 +1,16 @@
+'''
+
+PROBLEMA:
+
+Projete (design) uma animação de um foguete descendo e pousando na terra.
+
+Lembre-se de seguir a receita de Como Projetar Mundos e as demais!
+Não esqueça também de fazer a análise de domínio antes de começar a 
+trabalhar no código.
+
+Dica1: a parte da definição de dados já foi feita nos exercícios anteriores! Apenas revise-a e utilize no projeto.
+
+Utilize qualquer imagem de um foguete. Apenas lembre-se de colocar a imagem na mesma pasta que este código.
+
+
+'''
