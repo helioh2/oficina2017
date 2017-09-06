a9f54fc6cba7d22e489208d7f4949b177929e368
helioh4 <heliohenrique3@gmail.com>
2017-09-05 22:51:11 -0300

    Aula 05/09. (fix)

---
+++ exercicios3/semaforo.py
@@ -0,0 +1,17 @@
+'''
+PROBLEMA:
+Projete (design) uma animação de um semáforo.
+Seu programa deve mostrar um semáforo que é vermelho, depois verde,
+depois amarelo, depois vermelho, etc. Para este programa, a definição
+de dados do estado mutável do mundo deve ser uma enumeração.
+Para fazer as luzes mudarem a uma velocidade razoável, você pode usar
+a opção 'frequencia' do big_bang. Por exemplo, 'big_bang(inic, frequencia=1, ...)' fará o
+big-bang esperar 1 segundo antes de chamar a função no 'quando_tick'.
+Lembre-se de seguir a receita de Como Projetar Mundos e as demais!
+Não esqueça também de fazer a análise de domínio antes de começar a
+trabalhar no código.
+Dica1: a parte da definição de dados já foi feita em aula! Apenas revise-a e utilize no projeto.
+Dica2: o ideal é não utilizar a imagem como o valor do tipo de dado semáforo, mas apenas como a
+representação gráfica (como no caso do gato, que é apenas um número).
+Imagens de exemplo na pasta (vc pode colocar outras imagens se preferir)
+'''
