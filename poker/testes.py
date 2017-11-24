4907fa95b150bcdf52c8bb59fd4d44d710b3f5ce
helioh4 <heliohenrique3@gmail.com>
2017-11-24 19:59:46 -0200

    Aula 24/11.

--- poker/testes.py
+++ poker/testes.py
@@ -20,6 +20,15 @@ class TestePoker(unittest.TestCase):
         self.assertEqual( pega_valores(STRAIGHT_FLUSH), [7,8,9,10,11]  )
         self.assertEqual(pega_valores(FLUSH), [1, 2, 4, 7, 11])
 
+    def testStraight(self):
+        self.assertTrue(straight([13, 12, 11, 10, 9]))
+        self.assertFalse(straight([13, 11, 11, 10, 9]))
+
+    def testQuadra(self):
+        self.assertTrue(mesmo_tipo([9, 9, 9, 9, 3], 4))
+        self.assertFalse(mesmo_tipo([9, 9, 9, 8, 3], 4))
+        self.assertFalse(mesmo_tipo([9, 9, 9, 3, 3], 4))
+
 
 unittest.main()
 
